a8f8045586bf57528e0d58662d07212ddd87d9ec
王鼎文 <wangdingwen@wangdingwendeMacBook-Pro-2.local>
2025-12-28 13:45:15 +0800

    Fix: Clean dependencies and add sqlite hack

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import trafilatura
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')import trafilatura
 from langchain_core.documents import Document
 import streamlit as st
 import os
